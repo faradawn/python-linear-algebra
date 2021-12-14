277e97b33418543d7fb8d7861452b7b6f5f1353b
faradawn <faradawny@gmail,com>
2021-12-14 14:30:37 -0600

    update integral

--- python_stats/integral.py
+++ python_stats/integral.py
@@ -3,7 +3,7 @@ from sympy import *
 # declare your variables
 x, y, c = symbols("x y c")
 # enter your f(x,y) here
-f = 3/2*(x**2+y**2)
+f = 2*(x**3+y**3)
 # enter your inner bound for dx, then dy
 x_hi = 1
 x_low = 0
@@ -17,4 +17,8 @@ y_low = 0
 
 # calculate x's marginal density
 fx = integrate(f, (y, y_low, y_hi))
-print(fx)
+fx = integrate(x*fx, (x, x_low, x_hi))
+
+E_XY = integrate(x*y*f, (y, y_low, y_hi), (x, x_low, x_hi))
+
+print(E_XY)
